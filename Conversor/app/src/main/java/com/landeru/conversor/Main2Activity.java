ea719dc3d15787a7f18788e626166ddec9fb673b
LanderU <lander.usategui@gmail.com>
2016-02-14 00:44:39 +0100

    Conversor terminado

--- Conversor/app/src/main/java/com/landeru/conversor/Main2Activity.java
+++ Conversor/app/src/main/java/com/landeru/conversor/Main2Activity.java
@@ -6,24 +6,119 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main2);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        // Tratamiento de la etiqueta
+
+
+        TextView resultado = (TextView) findViewById(R.id.textView4);
+
+        // Controlamos de donde venimos para hacer la coversión adecuada.
+
+
+        Double dato = 0.0;
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+
+            dato = extras.getDouble("VACIO");
+
+        }
+
+        Bundle kiloKilo = getIntent().getExtras();
+
+        if (kiloKilo != null) {
+
+            dato = kiloKilo.getDouble("kkilobyte-kilobyte");
+        }
+
+        Bundle kiloMega = getIntent().getExtras();
+
+        if(kiloMega != null){
+
+            dato = kiloMega.getDouble("kilobyte-megabyte");
+            dato /= 1024;
+        }
+
+        Bundle kiloGiga = getIntent().getExtras();
+
+        if (kiloGiga != null){
+
+            dato = kiloGiga.getDouble("kilobyte-gigabyte");
+            dato /= 1048576;
+        }
+
+        Bundle megaKilo = getIntent().getExtras();
+
+        if (megaKilo != null){
+
+            dato = megaKilo.getDouble("megabyte-kilobyte");
+
+            dato *= 1024;
+        }
+
+        Bundle megaMega = getIntent().getExtras();
+
+        if(megaMega != null){
+
+            dato = megaKilo.getDouble("megabyte-megabyte");
+
+        }
+
+        Bundle megaGiga = getIntent().getExtras();
+
+        if(megaGiga != null){
+            dato = megaGiga.getDouble("megabyte-gigabyte");
+
+            dato /= 1024;
+
+        }
+
+        Bundle gigaKilo = getIntent().getExtras();
+
+        if (gigaKilo != null){
+
+            dato = gigaKilo.getDouble("gigabyte-kilobyte");
+
+            dato *= 1048576;
+
+        }
+
+        Bundle gigaMega = getIntent().getExtras();
+
+        if (gigaKilo != null){
+
+            dato = gigaMega.getDouble("gigabyte-megabyte");
+
+            dato *= 1024;
+
+        }
+
+        Bundle gigaGiga = getIntent().getExtras();
+
+        if (gigaKilo != null){
+
+            dato = gigaGiga.getDouble("gigabyte-gigabyte");
+
+        }
+
+
+        resultado.setText("La conversión es: "+ String.format("%.8f",dato));
+
+
     }
 
 }
--- Conversor/app/src/main/java/com/landeru/conversor/MainActivity.java
+++ Conversor/app/src/main/java/com/landeru/conversor/MainActivity.java
@@ -2,13 +2,19 @@ package com.landeru.conversor;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,12 +25,174 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        // Tratado para las etiquetas
+
+        TextView etiqueta1 = (TextView) findViewById(R.id.textView);
+        TextView etiqueta2 = (TextView) findViewById(R.id.textView2);
+        TextView etiqueta3 = (TextView) findViewById(R.id.textView3);
+
+        // Les damos nombre a las etiquetas
+
+        etiqueta1.setText("Origen");
+        etiqueta2.setText(("Cantidad a convertir"));
+        etiqueta3.setText("Destino");
+
+
+        // Tratamiento de RadioButton Grupo1
+
+        final RadioButton rb1 = (RadioButton) findViewById(R.id.radioButton);
+        final RadioButton rb2 = (RadioButton) findViewById(R.id.radioButton2);
+        final  RadioButton rb3 = (RadioButton) findViewById(R.id.radioButton3);
+
+        // Nombres a los RadioButton del primer grupo
+
+        rb1.setText("KiloBytes");
+        rb2.setText("MegaBytes");
+        rb3.setText("GigaByte");
+
+
+        // Dejamos uno de los RadioButton chequeados
+
+        rb1.setChecked(true);
+        rb2.setChecked(false);
+        rb3.setChecked(false);
+
+
+        // Tratamiento RadioButton Grupo2
+
+        final RadioButton rbg1 = (RadioButton) findViewById(R.id.radioButton4);
+        final RadioButton rbg2 = (RadioButton) findViewById(R.id.radioButton5);
+        final RadioButton rbg3 = (RadioButton) findViewById(R.id.radioButton6);
+
+
+        // Nombres para los RadioButton del segundo grupo
+
+        rbg1.setText("KiloBytes");
+        rbg2.setText("MegaBytes");
+        rbg3.setText("GigaByte");
+
+        // Dejamos uno de los RadioButton chequeados
+
+        rbg1.setChecked(true);
+        rbg2.setChecked(false);
+        rbg3.setChecked(false);
+
+
+        // EditText
+
+        final EditText entradaDatos = (EditText) findViewById(R.id.editText);
+
+
+        // Tratamiento del Button
+
+        Button convertir = (Button) findViewById(R.id.button);
+
+        // Darle nombre al botón
+
+        convertir.setText("Convertir");
+
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+
+                if(entradaDatos.getText().length() == 0){
+
+                    // No hay dato
+
+                    Intent i = new Intent(getApplicationContext(),Main2Activity.class);
+                    i.putExtra("VACIO", 0);
+                    startActivity(i);
+
+
+                }else if (rb1.isChecked() && rbg1.isChecked()){
+
+                    //kilobyte-->kilobyte
+                    String recogida = entradaDatos.getText().toString();
+                    Double pasoDato = new Double(recogida);
+                    Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+                    i.putExtra("kilobyte-kilobyte", pasoDato);
+                    startActivity(i);
+
+                } else if (rb1.isChecked() && rbg2.isChecked()){
+
+                    //kilobyte->MegaByte
+                    String recogida = entradaDatos.getText().toString();
+                    Double pasoDato = new Double(recogida);
+                    Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+                    i.putExtra("kilobyte-megabyte", pasoDato);
+                    startActivity(i);
+
+                } else if (rb1.isChecked() && rbg3.isChecked()){
+
+                    //kilobyte->GigaByte
+                    String recogida = entradaDatos.getText().toString();
+                    Double pasoDato = new Double(recogida);
+                    Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+                    i.putExtra("kilobyte-gigabyte", pasoDato);
+                    startActivity(i);
+
+                } else if (rb2.isChecked() && rbg1.isChecked()){
+
+                    //MegaByte--> KiloByte
+                    String recogida = entradaDatos.getText().toString();
+                    Double pasoDato = new Double(recogida);
+                    Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+                    i.putExtra("megabyte-kilobyte", pasoDato);
+                    startActivity(i);
+
+                } else if (rb2.isChecked() && rbg2.isChecked()){
+
+                    //MegaByte--> MegaByte
+                    String recogida = entradaDatos.getText().toString();
+                    Double pasoDato = new Double(recogida);
+                    Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+                    i.putExtra("megabyte-megabyte", pasoDato);
+                    startActivity(i);
+
+                } else if (rb2.isChecked() && rbg3.isChecked()){
+
+                    //MegaByte--> GigaByte
+                    String recogida = entradaDatos.getText().toString();
+                    Double pasoDato = new Double(recogida);
+                    Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+                    i.putExtra("megabyte-gigabyte", pasoDato);
+                    startActivity(i);
+
+                } else if (rb3.isChecked() && rbg1.isChecked()) {
+
+                    //GigaBytes-->Kilobytes
+                    String recogida = entradaDatos.getText().toString();
+                    Double pasoDato = new Double(recogida);
+                    Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+                    i.putExtra("gigabyte-kilobyte", pasoDato);
+                    startActivity(i);
+
+
+                } else if (rb3.isChecked() && rbg2.isChecked()) {
+
+                    //GigaByte--> MegaByte
+                    String recogida = entradaDatos.getText().toString();
+                    Double pasoDato = new Double(recogida);
+                    Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+                    i.putExtra("gigabyte-megabyte", pasoDato);
+                    startActivity(i);
+
+                } else if (rb3.isChecked() && rbg3.isChecked()){
+
+                    //GigaByte--> GigaByte
+                    String recogida = entradaDatos.getText().toString();
+                    Double pasoDato = new Double(recogida);
+                    Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+                    i.putExtra("gigabyte-gigabyte", pasoDato);
+                    startActivity(i);
+
+                }
+
+
             }
         });
+
     }
 
     @Override
