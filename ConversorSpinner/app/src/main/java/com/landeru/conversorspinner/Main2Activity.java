fcfdfa2b2716c3c6db9e63076ac1cacb7afe4c9b
LanderU <lander.usategui@gmail.com>
2016-02-21 16:48:50 +0100

    Terminado

--- ConversorSpinner/app/src/main/java/com/landeru/conversorspinner/Main2Activity.java
+++ ConversorSpinner/app/src/main/java/com/landeru/conversorspinner/Main2Activity.java
@@ -1,14 +1,22 @@
 package com.landeru.conversorspinner;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static com.landeru.conversorspinner.R.id.textView4;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private TextView resultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +33,94 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
 */
+
+        resultado = (TextView) findViewById(textView4);
+
+        Double dato = 0.0;
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null){
+
+            if (extras.containsKey("VACIO")){
+
+                dato = extras.getDouble("VACIO");
+
+                resultado.setText("La conversión es: "+ String.format("%.2f",dato));
+
+            }else if (extras.containsKey("bytes-bytes")){
+
+                dato = extras.getDouble("bytes-bytes");
+
+                resultado.setText("La conversión es de: "+ String.format("%.2f", dato));
+
+            }else if (extras.containsKey("bytes-kilobytes")){
+
+                dato = extras.getDouble("bytes-kilobytes");
+
+                dato /= 1024;
+
+                resultado.setText("La conversión es de: "+ String.format("%.8f", dato));
+
+            } else if (extras.containsKey("bytes-megabytes")){
+
+                dato = extras.getDouble("bytes-megabytes");
+
+                dato /= 1048576;
+
+                resultado.setText("La conversión es de: "+ String.format("%.8f", dato));
+
+            } else if (extras.containsKey("kilobytes-bytes")){
+
+                dato = extras.getDouble("kilobytes-bytes");
+
+                dato *= 1024;
+
+                resultado.setText("El resultado es: "+ String.format("%.2f",dato));
+
+            }else if(extras.containsKey("kilobytes-kilobytes")){
+
+                dato = extras.getDouble("kilobytes-kilobytes");
+
+                resultado.setText("El resultado es: "+ String.format("%.2f", dato));
+
+
+            } else if (extras.containsKey("kilobytes-megabytes")){
+
+                dato = extras.getDouble("kilobytes-megabytes");
+
+                dato /= 1024;
+
+                resultado.setText("El resultado es: "+ String.format("%.8f", dato));
+
+            }else if(extras.containsKey("megabytes-bytes")){
+
+                dato = extras.getDouble("megabytes-bytes");
+
+                dato *= 1048576;
+
+                resultado.setText("El resultado es: "+ String.format("%.2f", dato));
+
+            } else if (extras.containsKey("megabytes-kilobytes")){
+
+                dato = extras.getDouble("megabytes-kilobytes");
+
+                dato *= 1024;
+
+                resultado.setText("El resultado es: "+ String.format("%.2f", dato));
+
+            } else if (extras.containsKey("megabytes-megabytes")){
+
+                dato = extras.getDouble("megabytes-megabytes");
+
+                resultado.setText("El resultado es: "+ String.format("%.2f", dato));
+
+            }
+
+
+
+        }// end if
+
     }
 
 }
--- ConversorSpinner/app/src/main/java/com/landeru/conversorspinner/MainActivity.java
+++ ConversorSpinner/app/src/main/java/com/landeru/conversorspinner/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -22,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
     private Spinner spinnerOrigen;
     private Spinner spinnerDestino;
     private Button convertir;
+    private int posicionOrigen;
+    private int posicionDestino;
+
+    // Función para controlar el tipo de conversión a realizar
+    public void controlarAccion(String accion, Double dato){
+
+        Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+        i.putExtra(accion,dato);
+        startActivity(i);
+
+    }// controlarAccion
 
 
     @Override
@@ -59,16 +71,119 @@ public class MainActivity extends AppCompatActivity {
         spinnerOrigen = (Spinner) findViewById(R.id.spinner);
         spinnerOrigen.setAdapter(adaptador);
 
+        // Controlamos el tipo de dato de origen que hemos seleccionado
+        spinnerOrigen.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                // Nos quedamos con el número seleccionado.
+
+                posicionOrigen = position;
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
         spinnerDestino = (Spinner) findViewById(R.id.spinner2);
         spinnerDestino.setAdapter(adaptador);
 
+        // Tipo de dato de destino que hemos seleccionado
 
+        spinnerDestino.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                // Nos quedamos con el número seleccionado
+                posicionDestino = position;
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        introducirDato = (EditText) findViewById(R.id.editText);
 
         // Acción de pulsar el botón
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
+
+            // Nos quedamos con lo que haya dentro de EditText
+
+
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+
+                if (introducirDato.getText().length() == 0){
+
+                    controlarAccion("VACIO",0.0);
+
+                }else if (posicionOrigen == 0 && posicionDestino == 0){
+
+                    String datoRecogido = introducirDato.getText().toString();
+                    Double datoDoble = new Double(datoRecogido);
+
+                    controlarAccion("bytes-bytes", datoDoble);
+
+                }else if (posicionOrigen == 0 && posicionDestino == 1){
+
+                    String datoRecogido = introducirDato.getText().toString();
+                    Double datoDoble = new Double(datoRecogido);
+
+                    controlarAccion("bytes-kilobytes",datoDoble);
+
+                }else if (posicionOrigen == 0 && posicionDestino == 2){
+
+                    String datoRecogido = introducirDato.getText().toString();
+                    Double datoDoble = new Double(datoRecogido);
+
+                    controlarAccion("bytes-megabytes",datoDoble);
+
+                }else if (posicionOrigen == 1 && posicionDestino == 0){
+
+                    String datoRecogido = introducirDato.getText().toString();
+                    Double datoDoble = new Double(datoRecogido);
+
+                    controlarAccion("kilobytes-bytes",datoDoble);
+
+                }else if(posicionOrigen == 1 && posicionDestino ==1){
+
+                    String datoRecogido = introducirDato.getText().toString();
+                    Double datoDoble = new Double(datoRecogido);
+
+                    controlarAccion("kilobytes-kilobytes", datoDoble);
+
+                }else if (posicionOrigen == 1 && posicionDestino == 2){
+
+                    String datoRecogido = introducirDato.getText().toString();
+                    Double datoDoble = new Double(datoRecogido);
+
+                    controlarAccion("kilobytes-megabytes",datoDoble);
+
+                } else if (posicionOrigen == 2 && posicionDestino == 0){
+
+                    String datoRecogido = introducirDato.getText().toString();
+                    Double datoDoble = new Double(datoRecogido);
+
+                    controlarAccion("megabytes-bytes", datoDoble);
+
+                } else if (posicionOrigen == 2 && posicionDestino == 1){
+
+                    String datoRecogido = introducirDato.getText().toString();
+                    Double datoDoble = new Double(datoRecogido);
+
+                    controlarAccion("megabytes-kilobytes", datoDoble);
+
+                }else if (posicionOrigen == 2 && posicionDestino == 2){
+
+                    String datoRecogido = introducirDato.getText().toString();
+                    Double datoDoble = new Double(datoRecogido);
+
+                    controlarAccion("megabytes-megabytes", datoDoble);
+                }
+
             }
         });
 
